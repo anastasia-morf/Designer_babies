04289d946991880d1e48ef41805b72e30b8d96df
anastasia-morf <103618079+anastasia-morf@users.noreply.github.com>
2026-04-24 15:52:01 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/designer-babies/',
+  base: '/Designer_babies/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
