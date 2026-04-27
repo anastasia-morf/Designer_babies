0d322d8e8b019d50a82212aafd4998be8a18f5f9
anastasia-morf <103618079+anastasia-morf@users.noreply.github.com>
2026-04-28 09:49:21 +1000

    Update GameIntro.tsx

--- src/app/components/GameIntro.tsx
+++ src/app/components/GameIntro.tsx
@@ -5,7 +5,7 @@ import { GoogleFormEmbed } from './GoogleFormEmbed';
 
 // 🔧 Paste your "before" Google Form embed URL here:
 const PRE_GAME_FORM_URL =
-  'https://docs.google.com/forms/d/e/1FAIpQLSf5QxEV0HTOWarEzVSmi_Rp5DvUDzBfor-ygDldNDrjeAjFBA/viewform';
+  'https://docs.google.com/forms/d/e/1FAIpQLSdHggu03q2xATqYAY7rVoLkmndxIVr-oGcn_qGIMQLJK1bb0Q/viewform';
 
 interface GameIntroProps {
   onModeSelect: (mode: GameMode) => void;
